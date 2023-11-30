07ef51d56b2c3d0a017bff32089d7203f75e94f2
Nemanja Stankovic <necke.st@elfak.rs>
2023-11-30 22:10:51 +0100

    dodate slike cipova

--- snapshot.py
+++ snapshot.py
@@ -1,6 +1,10 @@
 import itertools
+
+import pygame
 import pygame as pg
 import sys
+from PIL import Image, ImageTk
+import tkinter as tk
 
 def get_even_positive_input():
     pg.init()
@@ -49,6 +53,10 @@ def get_even_positive_input():
 
         pg.display.flip()
 
+def load_and_scale_image(image_path, target_size):
+    image = pg.image.load(image_path)
+    return pg.transform.scale(image, target_size)
+
 if __name__ == '__main__':
 
     n = get_even_positive_input()
@@ -58,7 +66,7 @@ if __name__ == '__main__':
     BLACK = pg.Color(192, 192, 192)
     WHITE = pg.Color(105, 105, 105)
 
-    screen = pg.display.set_mode((1280, 720))
+    screen = pg.display.set_mode((1920, 1080))
     clock = pg.time.Clock()
 
     colors = itertools.cycle((WHITE, BLACK))
@@ -67,10 +75,24 @@ if __name__ == '__main__':
     width, height = n * tile_size, n * tile_size
     background = pg.Surface((width, height))
 
+
+    blackchip = load_and_scale_image("black-chip.png", (tile_size, tile_size))
+    whitechip = load_and_scale_image("white-chip.png", (tile_size, tile_size))
+
     for y in range(0, height, tile_size):
         for x in range(0, width, tile_size):
             rect = (x, y, tile_size, tile_size)
             pg.draw.rect(background, next(colors), rect)
+            if y > 0:
+                if y < height-tile_size:
+                    if (y - 1) % 2 == 0:
+                        if (x - 1) % 2 == 0:
+                            background.blit(blackchip, (x, y))
+            if y > 0:
+                if y < height-tile_size:
+                    if y % 2 == 0:
+                        if x % 2 == 0:
+                            background.blit(whitechip, (x, y))
         next(colors)
 
     game_exit = False
