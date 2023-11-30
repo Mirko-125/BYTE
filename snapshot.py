ecf3d1261d15deeaee9d3c8c18cb1ba067b6db39
Mirko <mirkoat21@gmail.com>
2023-11-30 23:51:46 +0100

    rewriten camel case

--- snapshot.py
+++ snapshot.py
@@ -2,19 +2,18 @@ import itertools
 import pygame as pg
 import sys
 
-#import graphFromMilos
 
-def get_even_positive_input():
+def getEvenPositiveInput():
     pg.init()
 
     # Set up the window
-    window_size = (400, 200)
-    screen = pg.display.set_mode(window_size)
+    windowSize = (400, 200)
+    screen = pg.display.set_mode(windowSize)
     pg.display.set_caption("BYTE | Set your dimensions")
 
     # Set up fonts
     font = pg.font.Font(None, 36)
-    input_text = ""
+    inputText = ""
 
     while True:
         for event in pg.event.get():
@@ -25,83 +24,84 @@ def get_even_positive_input():
                 if event.key == pg.K_RETURN:
                     # If Enter key is pressed, check if the input is a valid even positive number
                     try:
-                        number = int(input_text)
+                        number = int(inputText)
                         if number > 0 and number % 2 == 0:
                             pg.quit()
                             return number
                         else:
-                            # If not a valid input, clear the input_text
-                            input_text = ""
+                            # If not a valid input, clear the inputText
+                            inputText = ""
                     except ValueError:
-                        # If not a valid integer, clear the input_text
-                        input_text = ""
+                        # If not a valid integer, clear the inputText
+                        inputText = ""
                 elif event.key == pg.K_BACKSPACE:
                     # If Backspace key is pressed, remove the last character
-                    input_text = input_text[:-1]
+                    inputText = inputText[:-1]
                 else:
-                    # Append other keypresses to the input_text
-                    input_text += event.unicode
+                    # Append other keypresses to the inputText
+                    inputText += event.unicode
 
         # Draw the input prompt
         screen.fill((60, 70, 90))
-        prompt_text = font.render("Board size NxN, N is:", True, (200, 200, 200))
-        input_prompt = font.render(input_text, True, (220, 220, 220))
-        screen.blit(prompt_text, (50, 50))
-        screen.blit(input_prompt, (50, 100))
+        promptText = font.render("Board size NxN, N is:", True, (200, 200, 200))
+        inputPrompt = font.render(inputText, True, (220, 220, 220))
+        screen.blit(promptText, (50, 50))
+        screen.blit(inputPrompt, (50, 100))
 
         pg.display.flip()
 
-def load_and_scale_image(image_path, target_size):
-    image = pg.image.load(image_path)
-    return pg.transform.scale(image, target_size)
+
+def loadAndScaleImage(imagePath, targetSize):
+    image = pg.image.load(imagePath)
+    return pg.transform.scale(image, targetSize)
+
 
 def mainBoard(stack):
-    n = get_even_positive_input()
+    n = getEvenPositiveInput()
     pg.init()
 
-    BLACK = pg.Color(192, 192, 192)
-    WHITE = pg.Color(105, 105, 105)
+    black = pg.Color(192, 192, 192)
+    white = pg.Color(105, 105, 105)
 
     screen = pg.display.set_mode((1280, 720))
     clock = pg.time.Clock()
 
-    colors = itertools.cycle((WHITE, BLACK))
-    tile_size = 75
+    colors = itertools.cycle((white, black))
+    tileSize = 75
 
-    width, height = n * tile_size, n * tile_size
+    width, height = n * tileSize, n * tileSize
     background = pg.Surface((width, height))
 
-
-    blackchip = load_and_scale_image("black-chip.png", (tile_size, tile_size))
-    whitechip = load_and_scale_image("white-chip.png", (tile_size, tile_size))
+    blackChip = loadAndScaleImage("black-chip.png", (tileSize, tileSize))
+    whiteChip = loadAndScaleImage("white-chip.png", (tileSize, tileSize))
 
     c = 1
-    for y in range(0, height, tile_size):
-        for x in range(0, width, tile_size):
-            rect = (x, y, tile_size, tile_size)
+    for y in range(0, height, tileSize):
+        for x in range(0, width, tileSize):
+            rect = (x, y, tileSize, tileSize)
             pg.draw.rect(background, next(colors), rect)
-            if((x+y)%2==0):
-                stackPointer = stack #graph[c][1]
+            if (x + y) % 2 == 0:
+                stackPointer = stack  # graph[c][1]
                 print(c)
                 c += 1
                 # stack pointer spot
             if y > 0:
-                if y < height-tile_size:
+                if y < height - tileSize:
                     if (y - 1) % 2 == 0:
                         if (x - 1) % 2 == 0:
-                            background.blit(blackchip, (x, y))
+                            background.blit(blackChip, (x, y))
             if y > 0:
-                if y < height-tile_size:
+                if y < height - tileSize:
                     if y % 2 == 0:
                         if x % 2 == 0:
-                            background.blit(whitechip, (x, y))
+                            background.blit(whiteChip, (x, y))
         next(colors)
 
-    game_exit = False
-    while not game_exit:
+    gameExit = False
+    while not gameExit:
         for event in pg.event.get():
             if event.type == pg.QUIT:
-                game_exit = True
+                gameExit = True
 
         screen.fill((60, 70, 90))
         screen.blit(background, (300, 80))
@@ -111,9 +111,10 @@ def mainBoard(stack):
 
     pg.quit()
 
+
 if __name__ == '__main__':
     graph = {
-        #[]
+        # []
     }
     stack = [0]
 
