42d3c43c0264991bfbe3d9226fdcbfb0267965d7
Mirko <mirkoat21@gmail.com>
2023-12-02 03:40:39 +0100

    isWinner() function completed

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import sys
+
 from snapshot import *
 from graph import *
 from graphConstants import graphStack
+from player import Player
 
 def stackMove(graph,key,count,direction, interfaceTools):
     graph.move(key,count,direction)
@@ -12,11 +15,15 @@ if __name__ == '__main__':
     graph = Graph(n)
     graph.createTable()
     graph.move(5, 1, graph.DR)
-    graph.move(9, 1, graph.UR)
+    whitePlayer = Player("White")
+    blackPlayer = Player("Black")
+    whitePlayer.addPoints(2)
+
+    if (blackPlayer.isWinner()): # da je whitePlayer pobedio bi
+        sys.exit()
 
     for key, value in graph.nodes.items():
         print(f"Node {key}: {value[neighborNodes]}, {value[graphStack]}")
 
     interfaceTools = InterfaceTools(n)
     mainBoard(graph.nodes,interfaceTools)
-    #stackMove(graph, 5, 4, graph.DR, interfaceTools)
