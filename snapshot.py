1ca1a9165039c7b94a8beaa58749949e54d2172e
Mirko <mirkoat21@gmail.com>
2023-12-08 19:42:28 +0100

    Fixing movement bugs

--- snapshot.py
+++ snapshot.py
@@ -116,12 +116,13 @@ def drawTable(graph,interfaceTools):
                 # stack pointer spot
         next(colors)
 
-def movementHandle(c, stack, graph, state, interfaceTools):
-    # False je state 1, True je state 2
+def movementHandle(cRect, stack, graph, state, interfaceTools):
     if not stack.isEmpty() and state:
         # Use legalMoves to return all keys with valid places to move
         # Use those keys to highlight appropriate rectangles on the field
         state = False
+        for n in graph.nodes[cRect['nodeKey']]['neighborNodes']:
+            drawPossibleMove(n,graph) # ne znam kako da pristupim grafu preko keya
         while not state:
             for event in pg.event.get():
                 if event.type == pg.QUIT:
@@ -133,28 +134,50 @@ def movementHandle(c, stack, graph, state, interfaceTools):
                     for y in range(10, interfaceTools.height, interfaceTools.tileSize):
                         for x in range(10, interfaceTools.width, interfaceTools.tileSize):
                             if (x + y) % 2 == 0:
-                                rectInfo = {}
+                                bRect = {}
                                 rect = pg.Rect(x, y, interfaceTools.tileSize, interfaceTools.tileSize)  # Maybe
 
-                                rectInfo["rect"] = rect
-                                rectInfo["nodeKey"] = b
+                                bRect["rect"] = rect
+                                bRect["nodeKey"] = b
+
+                                bRect["x"] = x
+                                bRect["y"] = y
+
                                 b += 1
-                                if rectInfo["rect"].collidepoint(mouse_x, mouse_y):
-                                    print("Starting coordinates:", x, y)
-                                    print(f"B is {rectInfo['nodeKey']}")
-                                    if c['nodeKey'] == rectInfo['nodeKey']:
-                                        print("it's okay now")
+                                if bRect["rect"].collidepoint(mouse_x, mouse_y):
+                                    #print("Starting coordinates:", x, y)
+                                    print(f"B is {bRect['nodeKey']}")
+                                    if cRect['nodeKey'] == bRect['nodeKey']:
+                                        print("you can exit the program.")
                                         return
                                     else:
-                                        print(graph.nodes[c['nodeKey']]['neighborNodes'])
-                                        for i in graph.nodes[c['nodeKey']]['neighborNodes']:
-                                            pass
-                                            #nacrtaj tacku
-                                # drawTable(graph,interfaceTools)
-        print(graph.nodes[c['nodeKey']])
+                                        if graph.nodes[cRect['nodeKey']]['neighborNodes'][0] == bRect['nodeKey'] and not None:
+                                            graph.move(cRect['nodeKey'],1,graph.UR)
+                                            return
+                                        elif graph.nodes[cRect['nodeKey']]['neighborNodes'][1] == bRect['nodeKey'] and not None:
+                                            graph.move(cRect['nodeKey'], 1, graph.UL)
+                                            return
+                                        elif graph.nodes[cRect['nodeKey']]['neighborNodes'][2] == bRect['nodeKey'] and not None:
+                                            graph.move(cRect['nodeKey'],1,graph.DL)
+                                            return
+                                        elif graph.nodes[cRect['nodeKey']]['neighborNodes'][3] == bRect['nodeKey'] and not None:
+                                            graph.move(cRect['nodeKey'],1,graph.DR)
+                                            return
+                                        '''
+                                        for x in graph.nodes[cRect['nodeKey']]['neighborNodes']:
+                                            if x == bRect['nodeKey']:
+                                                
+                                        pass # prodjes kroz petlju bRecta i ako je b == susedu c onda je potez dozvoljen
+                                        '''
+
+        print(graph.nodes[cRect['nodeKey']])
         #graph.move(c['nodeKey'],1,graph.DR)
 
     print("goodbye.")
+    pass
+
+def drawPossibleMove(n,graph):
+    print(graph.nodes[n])
     pass
 def mainBoard(graph, interfaceTools):
     pg.init()
@@ -201,6 +224,10 @@ def mainBoard(graph, interfaceTools):
 
                             rectInfo["rect"] = rect
                             rectInfo["nodeKey"] = c
+
+                            rectInfo["x"] = x
+                            rectInfo["y"] = y
+
                             c+=1
                             if rectInfo["rect"].collidepoint(mouse_x, mouse_y):
                                 print("Starting coordinates:", x, y)
