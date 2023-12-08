b934950b8ac9f4b434ad77881fd54954584a977f
Mirko <mirkoat21@gmail.com>
2023-12-08 21:46:42 +0100

    redone main

--- main.py
+++ main.py
@@ -11,19 +11,27 @@ def stackMove(graph,key,count,direction, interfaceTools):
     drawChips(interfaceTools, graph.nodes[direction(key)][graphStack])
 
 if __name__ == '__main__':
-    n = getEvenPositiveInput()
+    n = prompt()
     graph = Graph(n)
     graph.createTable()
     #graph.move(5, 1, graph.DR)
     whitePlayer = Player("White")
     blackPlayer = Player("Black")
-    whitePlayer.addPoints(2)
-    if (whitePlayer.isWinner()):
-        pg.quit()
-    if (blackPlayer.isWinner()): # da je whitePlayer pobedio bi
-        sys.exit()
+    #whitePlayer.addPoints(2)
+    #if (whitePlayer.isWinner()):
+        #pg.quit()
+    #if (blackPlayer.isWinner()): # da je whitePlayer pobedio bi
+        #sys.exit()
     #for key, value in graph.nodes.items():
         #print(f"Node {key}: {value[neighborNodes]}, {value[graphStack]}")
-
-    interfaceTools = InterfaceTools(n)
-    mainBoard(graph,interfaceTools)
+    points = 0
+    while whitePlayer.isWinner() is False or blackPlayer.isWinner() is False:
+        interfaceTools = InterfaceTools(n)
+        if mainBoard(graph, interfaceTools): # mozda cemo morati da vratimo tuple umesto Tru/Flase
+            points += 1
+            whitePlayer.addPoints(points)
+        else:
+            points += 1
+            blackPlayer.addPoints(points)
+        points = 0
+    pg.quit()
